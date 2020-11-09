8e085daac6e320feff54d9b7a211e35d855c27f6
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2020-11-10 01:36:28 +0200

    Добавлена задача 1.3

---
+++ task_0003/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace task_0003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вывести на экран числа 50 и 10 одно под другим.
+
+        }
+    }
+}
