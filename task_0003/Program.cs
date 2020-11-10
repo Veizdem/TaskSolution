4afc7ef845c477d1f81a51995fffbadd38823787
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2020-11-10 07:32:21 +0200

    Добавлено решение задачи 1.3

--- task_0003/Program.cs
+++ task_0003/Program.cs
@@ -8,6 +8,13 @@ namespace task_0003
         {
             // Вывести на экран числа 50 и 10 одно под другим.
 
+            int a = 50, b = 10;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            Console.ReadKey();
+
         }
     }
 }
