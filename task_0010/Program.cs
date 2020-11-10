9f508dbeb7547127051b96395faa8bf6cc4072ce
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2020-11-10 08:04:35 +0200

    Добавлено решение задачи 1.10

--- task_0010/Program.cs
+++ task_0010/Program.cs
@@ -8,6 +8,14 @@ namespace task_0010
         {
             // Составить программу, которая запрашивает имя человека
             // и повторяет его на экране.
+
+            Console.WriteLine("Введите Ваше имя:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("{0}", name);
+
+            Console.ReadKey();
+
         }
     }
 }
