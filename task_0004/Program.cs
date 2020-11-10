4632a25b41c5f56ba3006670e97a2f6e419f1b7a
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2020-11-10 07:35:25 +0200

    Добавление задачи 1.4

---
+++ task_0004/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task_0004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вывести на экран числа 5, 10 и 21 одно под другим.
+        }
+    }
+}
