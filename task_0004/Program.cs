614e1de855ae9ff60a96b097b9cc194922008de7
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2020-11-10 07:36:56 +0200

    Добавление решения задачи 1.4

--- task_0004/Program.cs
+++ task_0004/Program.cs
@@ -7,6 +7,15 @@ namespace task_0004
         static void Main(string[] args)
         {
             // Вывести на экран числа 5, 10 и 21 одно под другим.
+
+            int a = 5, b = 10, c = 21;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+
         }
     }
 }
