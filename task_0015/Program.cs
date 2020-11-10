b7416e33a86b787cca9d0fce96811dbbc8302084
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2020-11-10 09:09:12 +0200

    Добавлено решение задачи 1.15

--- task_0015/Program.cs
+++ task_0015/Program.cs
@@ -9,6 +9,18 @@ namespace task_0015
             // Составить программу вывода на экран в одну строку четырех\
             // любых чисел, вводимых с клавиатуры, с одним пробелом между ними.
 
+            Console.Write("Введите первое число: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите третье число: ");
+            int n3 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите четвертое число: ");
+            int n4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0} {1} {2} {3}", n1, n2, n3, n4);
+
+            Console.ReadKey();
 
         }
     }
