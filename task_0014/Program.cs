03d012b3c3c11b2b9962acaa6558c14ec7993fdc
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2020-11-10 08:42:05 +0200

    Добавлено решение задачи 1.14

--- task_0014/Program.cs
+++ task_0014/Program.cs
@@ -9,6 +9,16 @@ namespace task_0014
             // Составить программу вывода на экран в одну строку трех любых
             // чисел, вводимых с клавиатуры, с двумя пробелами между ними.
 
+            Console.Write("Введите первое число: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите третье число: ");
+            int n3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0}  {1}  {2}", n1, n2, n3);
+
+            Console.ReadKey();
         }
     }
 }
