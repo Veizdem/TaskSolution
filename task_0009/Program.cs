6f1d9c7f0e5de2a03edb93293711af2bd080285b
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2020-11-10 07:57:35 +0200

    Добавлено решение задачи 1.9

--- task_0009/Program.cs
+++ task_0009/Program.cs
@@ -10,6 +10,14 @@ namespace task_0009
             // с клавиатуры. После выводимого числа должно следовать
             // сообщение «– вот какое число Вы ввели».
 
+            string stringNumber = Console.ReadLine();
+
+            int number = Convert.ToInt32(stringNumber);
+
+            Console.WriteLine("{0} - вот какое число Вы ввели", number);
+
+            Console.ReadKey();
+
         }
     }
 }
