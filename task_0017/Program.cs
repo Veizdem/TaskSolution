a4b94390da84cedfbcd9495238731b6c8c2d92dc
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2020-11-10 09:44:47 +0200

    Добалено решение задачи 1.17

--- task_0017/Program.cs
+++ task_0017/Program.cs
@@ -16,7 +16,26 @@ namespace task_0017
              * вместо имен величин.
              */
 
+            Console.Write("Введите переменную a: ");
+            string sa = Console.ReadLine();
+            int a = Convert.ToInt32(sa);
 
+            Console.Write("Введите переменную b: ");
+            string sb = Console.ReadLine();
+            int b = Convert.ToInt32(sb);
+
+            Console.Write("Введите переменную x: ");
+            string sx = Console.ReadLine();
+            int x = Convert.ToInt32(sx);
+
+            Console.Write("Введите переменную y: ");
+            string sy = Console.ReadLine();
+            int y = Convert.ToInt32(sy);
+
+            Console.WriteLine("a) 2 кг\tб) {0} 1\tв) {1} {2}", a, x, y);
+            Console.WriteLine("  13 17\t   19 {0}\t   5 {1}", b, y);
+
+            Console.ReadKey();
         }
     }
 }
