43436ebb12ffa0a902b8872c91904035ec173d86
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2020-11-10 07:58:45 +0200

    Добавлена задача 1.10

---
+++ task_0010/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace task_0010
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Составить программу, которая запрашивает имя человека
+            // и повторяет его на экране.
+        }
+    }
+}
