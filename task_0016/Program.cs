fd4e7be566d84a7622a58d4b52e7212763ba2e14
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2020-11-10 09:40:51 +0200

    Добавлено решение задачи 1.16

--- task_0016/Program.cs
+++ task_0016/Program.cs
@@ -16,6 +16,26 @@ namespace task_0016
              * вместо имен величин.
              */
 
+            Console.Write("Введите переменную t: ");
+            string st = Console.ReadLine();
+            int t = Convert.ToInt32(st);
+
+            Console.Write("Введите переменную v: ");
+            string sv = Console.ReadLine();
+            int v = Convert.ToInt32(sv);
+
+            Console.Write("Введите переменную x: ");
+            string sx = Console.ReadLine();
+            int x = Convert.ToInt32(sx);
+
+            Console.Write("Введите переменную y: ");
+            string sy = Console.ReadLine();
+            int y = Convert.ToInt32(sy);
+
+            Console.WriteLine("a) 5 10\tб) 100  {0}\tв) {1} 25", t, x);
+            Console.WriteLine("   7 см\t   1949 {0}\t   {1} {2}", v, x, y);
+
+            Console.ReadKey();
 
         }
     }
