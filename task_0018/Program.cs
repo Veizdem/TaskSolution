42a1576c4f4d9897cfe899f3069ae9d3fd9a7fcc
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2020-11-18 17:29:20 +0200

    Добавлено решение задачи 2.1

--- task_0018/Program.cs
+++ task_0018/Program.cs
@@ -9,11 +9,35 @@ namespace task_0018
 
             /* Составить программу:
              * 
-             * а) вычисления значения функции y = 17x2 – 6x + 13 при любом значении х;
+             * а) вычисления значения функции y = 17x^2 – 6x + 13 при любом значении х;
              * 
-             * б) вычисления значения функции y = 3a2 + 5a – 21 при любом значении а;
+             * б) вычисления значения функции y = 3a^2 + 5a – 21 при любом значении а;
              * 
              */
+
+            Console.WriteLine("Решение пункта а)\n");
+
+            Console.Write("X = ");
+            string stringX = Console.ReadLine();
+            double x = Convert.ToDouble(stringX);
+
+            double y = 17 * Math.Pow(x, 2) - 6 * x + 13;
+
+            Console.WriteLine("При х = {0}, y = 17x^2 – 6x + 13 = {1}", x, y);
+
+            Console.WriteLine(new string('-', 50));
+
+            Console.WriteLine("\nРешение пункта б)\n");
+
+            Console.Write("A = ");
+            string stringA = Console.ReadLine();
+            double a = Convert.ToDouble(stringA);
+
+            y = 3 * Math.Pow(a, 2) + 5 * a - 21;
+
+            Console.WriteLine("При a = {0}, y = 3a^2 + 5a – 21 = {1}", a, y);
+
+            Console.ReadKey();
         }
     }
 }
