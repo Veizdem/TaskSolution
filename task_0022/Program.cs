3a310eeca5db6ced1e2f7b9508aed26c956f5490
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2020-11-18 18:07:36 +0200

    Добавлена задача 2.5

---
+++ task_0022/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task_0022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Дан радиус окружности. Найти ее диаметр.
+        }
+    }
+}
