c4b288a0c7f4ec9129f3ce19cba211107460de74
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2020-11-18 18:00:02 +0200

    Добавлено решение задачи 2.3

--- task_0020/Program.cs
+++ task_0020/Program.cs
@@ -12,6 +12,30 @@ namespace task_0020
              * 
              * Функции в приложенных картинках
              */
+
+            Console.WriteLine("Решение пункта а)\n");
+
+            Console.Write("A = ");
+            string stringA = Console.ReadLine();
+            double a = Convert.ToDouble(stringA);
+
+            double y = Math.Sqrt((2 * a + Math.Sin(Math.Abs(3 * a))) / 3.56);
+
+            Console.WriteLine("При а = {0} выражение равно {1}", a, y);
+
+            Console.WriteLine(new string('-', 50));
+
+            Console.WriteLine("\nРешение пункта б)\n");
+
+            Console.Write("X = ");
+            string stringX = Console.ReadLine();
+            double x = Convert.ToDouble(stringX);
+
+            y = Math.Sin((3.2 + Math.Sqrt(1 + x)) / Math.Abs(5 * x));
+
+            Console.WriteLine("При х = {0} выражение равно {1}", x, y);
+
+            Console.ReadKey();
         }
     }
 }
