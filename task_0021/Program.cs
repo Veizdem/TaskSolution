5a023e36240f36339bdc4676592357f03b120181
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2020-11-18 18:06:43 +0200

    Добавлено решение задачи 2.4

--- task_0021/Program.cs
+++ task_0021/Program.cs
@@ -7,6 +7,16 @@ namespace task_0021
         static void Main(string[] args)
         {
             // Дана сторона квадрата. Найти его периметр.
+
+            Console.Write("Сторона квадрата: ");
+            string stringSide = Console.ReadLine();
+            double side = Convert.ToDouble(stringSide);
+
+            double perimeter = side * 4;
+
+            Console.WriteLine("Периметр квадрата со стороной {0} равен {1}.", side, perimeter);
+
+            Console.ReadKey();
         }
     }
 }
