ea2ff599cf239b4e23bf11c8569b4ff9429101f1
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2020-11-18 18:10:51 +0200

    Добавлено решение задачи 2.5

--- task_0022/Program.cs
+++ task_0022/Program.cs
@@ -7,6 +7,16 @@ namespace task_0022
         static void Main(string[] args)
         {
             // Дан радиус окружности. Найти ее диаметр.
+
+            Console.Write("Введите радиус окружности: ");
+            string stringRadius = Console.ReadLine();
+            double radius = Convert.ToDouble(stringRadius);
+
+            double diameter = radius * 2;
+
+            Console.WriteLine("Диаметр окружности с радиусом {0} равен {1}", radius, diameter);
+
+            Console.ReadKey();
         }
     }
 }
