630ceea08552b51cf503b05f58bf644860952e5f
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2020-11-18 17:39:10 +0200

    Добавлено решение задачи 2.2

--- task_0019/Program.cs
+++ task_0019/Program.cs
@@ -10,6 +10,16 @@ namespace task_0019
              * 
              * (Функция прикреплена в картинке)
              */
+
+            Console.Write("А = ");
+            string stringA = Console.ReadLine();
+            double a = Convert.ToDouble(stringA);
+
+            double y = (Math.Pow(a, 2) + 10) / Math.Sqrt(Math.Pow(a, 2) + 1);
+
+            Console.WriteLine("При а = {0} результат равен {1}", a, y);
+
+            Console.ReadKey();
         }
     }
 }
