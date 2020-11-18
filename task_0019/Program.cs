9f97e4ac4f9267de5def5f42c9e799ab15abf008
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2020-11-18 17:33:22 +0200

    Добавлена задача 2.2

---
+++ task_0019/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace task_0019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Составить программу вычсиления фунции при любом значении а.
+             * 
+             * (Функция прикреплена в картинке)
+             */
+        }
+    }
+}
