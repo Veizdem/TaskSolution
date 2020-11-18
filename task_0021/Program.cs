47d26086d80a05dc585b740be5eb137ac965db5e
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2020-11-18 18:01:19 +0200

    Добавлена задача 2.4

---
+++ task_0021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task_0021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Дана сторона квадрата. Найти его периметр.
+        }
+    }
+}
